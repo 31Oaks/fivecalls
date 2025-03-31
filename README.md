5d893a0cb20b9a0f470b6c2f8e2ff2d1913f5f4b
Bryan Shepherd <thespamhouse@gmail.com>
2025-03-31 16:26:36 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+To do:
+- ~~Add filters to show all categories, top 5, and top 10~~
+- add functionality to save all data in a backup repo and keep only the last month in the main repo
+- add auto commit and push to git with cron
+- add a table with chart information
+- add button to switch between last 48 hours, last 7 days, and last month
+- make display times eastern
+- prettify text on charts a bit
+- add cron tab creation to a python script?
+
+How to install:
+
+1. Create a 'fivecalls' user on the host system; log into that account
+
+2. Create the python virtual environment. In a terminal:
+
+# Create a directory for your project (optional)
+mkdir ~/my_project
+cd ~/my_project
+
+# Create the virtual environment
+python3 -m venv ".fivecalls"
+
+3. Activate the environment
+source .fivecalls/bin/activate
+
+4. Install dependencies
+pip install -r requirements.txt
+
+5. Create .tmp directory in project file if it doesn't exist
+
+6. Create cron job in editor
+
+crontab -e
+
+In the file editor that opens, something along the lines of the below, depending on how often the scripts should run:
+
+0 * * * * cd /home/fivecalls/Projects/fivecalls && /bin/bash -c 'source .fivecalls/bin/activate && python fetch_data.py && deactivate >> .tmp/cron_fetch_data_logfile.log 2>&1'
+0 */2 * * * cd /home/fivecalls/Projects/fivecalls && /bin/bash -c 'source .fivecalls/bin/activate && python create_charts.py && deactivate >> .tmp/cron_create_charts_logfile.log 2>&1'
+
+
+
---
+++ fetch_data.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import json
+import sqlite3
+from datetime import datetime
+from datetime import timezone
+import pandas as pd
+
+url = 'https://api.5calls.org/v1/issues'
+
+backup_path = '/home/fivecalls/Projects/fivecallsdatabackup/'
+
+def parse_json_to_db(data, utc_time, db_name="fivecalls.db", table_name="flat_data", backup_path=backup_path):
+    os.makedirs("data", exist_ok=True)
+    os.makedirs(backup_path, exist_ok=True)
+    df = pd.json_normalize(data)
+    df['inserted_at_utc'] = utc_time
+    df['categories'] = df['categories'].apply(json.dumps)
+    df['contactAreas'] = df['contactAreas'].apply(json.dumps)
+    df['outcomeModels'] = df['outcomeModels'].apply(json.dumps)
+    if 'stats.calls' in df.columns:
+        df.rename(columns={'stats.calls': 'calls'}, inplace=True)
+    
+    # Write data to the charting database ('data') and the backup database ('backup_path')
+    for path in ('data/', backup_path):
+        db_name_tmp = os.path.join(path, db_name)
+        print(f"Writing to {db_name_tmp}...")
+        with sqlite3.connect(db_name_tmp) as conn:
+            df.to_sql(table_name, conn, if_exists='append', index=False)
+        
+        # Remove only keep one month of data in charting db
+        if path == 'data/':
+            conn.execute(f"""
+                DELETE FROM {table_name}
+                WHERE datetime(created_at) < datetime('now', '-1 month', '-1 day')
+            """)
+
+        print(f"✅ Appended {len(df)} records to {table_name} in {db_name_tmp}.")
+
+def fetch_and_store(keep_original_data=True, backup_path=backup_path):
+    response = requests.get(url)
+    utc_time = datetime.now(timezone.utc).isoformat()
+    if response.status_code == 200:
+        data = response.json()
+        parse_json_to_db(data, utc_time)
+        if keep_original_data:
+            backup_path = os.path.join(backup_path, 'original_data')
+            os.makedirs(backup_path, exist_ok=True)
+            with open(f'{backup_path}/data_{utc_time}.json', 'w') as f:
+                json.dump(data, f)
+            print(f"✅ Data saved at {utc_time}")
+    else:
+        print(f"❌ API Error: {response.status_code}")
+
+if __name__ == "__main__":
+    fetch_and_store()
